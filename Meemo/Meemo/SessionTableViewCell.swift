93ba65a3be8c4d96ef2dcf041ddd291925e89bae
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2017-05-22 21:46:02 +0200

    TableViewDataSource and Delegate implemented for SessionsListViewController

---
+++ Meemo/Meemo/SessionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SessionTableViewCell.swift
+//  Meemo
+//
+//  Created by Daniel Lohse on 5/22/17.
+//  Copyright © 2017 Superstudio. All rights reserved.
+//
+
+import UIKit
+
+class SessionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Meemo/Meemo/SessionsListViewController.swift
+++ Meemo/Meemo/SessionsListViewController.swift
@@ -10,9 +10,50 @@ import UIKit
 
 class SessionsListViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.separatorStyle = .none
+        
+        tableView.backgroundColor = UIColor.clear
+        tableView.isOpaque = false
+        tableView.backgroundView = nil
+
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+    
+}
+
+extension SessionsListViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "sessionCell", for: indexPath) as! SessionTableViewCell
+//        let session = self.course?.sessions[indexPath.row]
+//        cell.name.text = session!.name
+//        cell.isSessionlocked((session?.locked)!)
+//        
+//        cell.selectionStyle = UITableViewCellSelectionStyle.none
+        
+        return cell
+    }
+
+}
+
+extension SessionsListViewController: UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     
 }

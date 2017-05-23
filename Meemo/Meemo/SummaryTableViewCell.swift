50af23c63602fcc16b63fae5d83c4c6136628b9f
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2017-05-23 14:17:00 +0200

    SummaryTableViewCell added and Advice model integrated

---
+++ Meemo/Meemo/SummaryTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  SummaryTableViewCell.swift
+//  Meemo
+//
+//  Created by Daniel Lohse on 5/23/17.
+//  Copyright © 2017 Superstudio. All rights reserved.
+//
+
+import UIKit
+
+class SummaryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var numberImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    
+    var advice: Advice?{
+        didSet{
+            self.updateUI()
+        }
+    }
+    
+    private func updateUI(){
+        
+        if let advice = advice {
+            title.text = advice.title
+            descriptionLabel.text = advice.description
+            numberImage.image = advice.numberImage()
+            
+        }else{
+            descriptionLabel.text = nil
+            title.text = nil
+            numberImage = nil
+        }
+        
+    }
+
+}
